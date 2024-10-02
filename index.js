415b83fdea4308716a16d69447e42c7fe6ea187f
Sangida Ahmed <sangida.work@gmail.com>
2024-10-02 12:56:04 -0400

    added question 2

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const fiveToOneHundred = () => {
 }
 
 fiveToOneHundred();
+
+const multiplesOfThree = () => {
+    for ( let i = 3; i <=100; i++)
+        if (i % 3 === 0) {console.log(i)}
+}
+multiplesOfThree();
